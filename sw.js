025696b333b5622166db15274b1c4aca2ac65b9a
acutepainservice <acutepainservicehpusm@gmail.com>
2026-09-07 10:15:15 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'aps-rounds-v1.1.6';
+const SHELL = ['./','./index.html','./manifest.webmanifest','./medications.js','./icon-192.png','./icon-512.png','./icon-maskable-192.png','./icon-maskable-512.png','./apple-touch-icon.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (event.request.method !== 'GET' || !url.origin.includes(self.location.origin)) return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(event.request, copy));
+      return resp;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
